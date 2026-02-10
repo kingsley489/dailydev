2fe5d28bf97a4f7da5c02dc114fd9c257dcabee7
kingsley489 <kingsleydesigns37@gmail.com>
2026-02-09 13:06:30 -1200

    import mugshot image

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import mugshotImage from './assets/kingsleyt.jpg';
 import React, { useState, useEffect } from 'react';
 import { 
   Paperclip, 
@@ -388,10 +389,10 @@ const App: React.FC = () => {
               <PaperClip className="left-1/2 -translate-x-1/2 -top-4" />
               <div className="aspect-square w-full bg-gray-200 mb-4 overflow-hidden relative grayscale contrast-125">
                 <img 
-                  src="/dailydev/kingsleyt.jpg" 
-                  alt="Suspect Mugshot" 
-                  className="w-full h-full object-cover"
-                />
+             src={mugshotImage}
+              alt="Suspect Mugshot" 
+              className="w-full h-full object-cover"
+                />  
                 {/* Scanlines */}
                 <div className="absolute inset-0 bg-[linear-gradient(rgba(18,16,16,0)_50%,rgba(0,0,0,0.25)_50%),linear-gradient(90deg,rgba(255,0,0,0.06),rgba(0,255,0,0.02),rgba(0,0,255,0.06))] z-10 bg-[length:100%_4px,6px_100%] pointer-events-none"></div>
               </div>
